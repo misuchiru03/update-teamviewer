460778a0aa760781de038dc04c19ac9f4d9a86c6
misuchiru03 <misuchiru03@gmail.com>
2018-01-26 15:11:23 -0600

    Update update-teamviewer.sh
    Fixed version searches with curl for arm arch.

--- update-teamviewer.sh
+++ update-teamviewer.sh
@@ -27,12 +27,12 @@ if [[ `uname -a | egrep "amd64|x86_64"` ]]; then
                         ARCH='amd64'
                 else
                         DEB=1
-			EXT='deb'
+                        EXT='deb'
                         ARCH='amd64'
                 fi
         else
                 RPM=1
-		EXT='rpm'
+                EXT='rpm'
                 ARCH='x86_64'
         fi
 elif [[ `uname -a | egrep "i386|i686"` ]]; then
@@ -51,12 +51,12 @@ elif [[ `uname -a | egrep "i386|i686"` ]]; then
                         ARCH='i386'
                 else
                         DEB=1
-			EXT='deb'
+                        EXT='deb'
                         ARCH='i386'
                 fi
         else
                 RPM=1
-		EXT='rpm'
+                EXT='rpm'
                 ARCH='i686'
         fi
 else
@@ -75,12 +75,12 @@ else
                         ARCH='armhf'
                 else
                         DEB=1
-			EXT='deb'
+                        EXT='deb'
                         ARCH='armhf'
                 fi
         else
                 RPM=1
-		EXT='rpm'
+                EXT='rpm'
                 ARCH='armv7hl'
         fi
 fi
@@ -88,12 +88,13 @@ fi
 # Remove comment for debugging to test variables
 #echo -e "DEB=$DEB\nRPM=$RPM\nOTHER=$OTHER\nARCH=$ARCH\nEXT=$EXT" && exit
 
-
-# Other distro test
-
-
 # Check the most up-to-date version from teamviewer.com and set variable
-NEWESTVERSION=$(curl -s https://www.teamviewer.com/en/download/linux/ | grep -8 "*\." | grep $EXT | head -n 1 | cut -dv -f2 | cut -d'<' -f1 | cut -d' ' -f1)
+# Check ARM version
+if [[ "$ARCH" == "arm" ]]; then
+	NEWESTVERSION=$(curl -s https://www.teamviewer.com/en/download/linux/ | grep -14 $ARCH | head -n 1 | cut -dv -f2 | cut -d'<' -f1 | cut -d' ' -f1)
+else
+	NEWESTVERSION=$(curl -s https://www.teamviewer.com/en/download/linux/ | grep -8 "*\." | grep $EXT | head -n 1 | cut -dv -f2 | cut -d'<' -f1 | cut -d' ' -f1)
+fi
 
 # Check currently installed version and set variable
 CURRENTVERSION=$(teamviewer -version | grep TeamViewer | awk '{ print $4 }')
@@ -103,7 +104,7 @@ echo -e "Newest Version: \t$NEWESTVERSION"
 
 if [ "$CURRENTVERSION" \< "$NEWESTVERSION" ]; then
         echo "There is an updated version."
-#	exit 0   ###This line is for debugging purposes
+#       exit 0   ###This line is for debugging purposes
 else
         echo "You already have the most updated version."
         exit 0
@@ -111,19 +112,35 @@ fi
 echo ''
 
 # Setting the Teamviewer Filename we will use to download and install
-TV_FILENAME="teamviewer_$NEWESTVERSION_$ARCH.$EXT"
+if [[ "$ARCH" =~ ^(arm) ]]
+	TV_FILENAME="teamviewer-host.$ARCH.$EXT"
+else
+	TV_FILENAME="teamviewer_$ARCH.$EXT"
+fi
 
-wget $(curl -s https://www.teamviewer.com/en/download/linux/ | grep -8 $NEWESTVERSION | grep linkBlue | grep $ARCH\.$EXT | cut -d'"' -f4) -O $USRHOME/Downloads/$TV_FILENAME
+wget $(curl -s https://www.teamviewer.com/en/download/linux/ | grep -8 "$NEWESTVERSION" | grep $TV_FILENAME | cut -d'"' -f4) -O $USRHOME/Downloads/$TV_FILENAME
 
 # Install the package
 if [ $EXT == 'deb' ]; then
-	sudo apt install -y $USRHOME/Downloads/$TV_FILENAME
+        sudo apt install -y $USRHOME/Downloads/$TV_FILENAME
+elif [ $EXT == 'rpm' ]; then
+        sudo yum install -y $USRHOME/Downloads/$TV_FILENAME
+	### Not tested yet.  If you test it, and it doesn't work, please give me some feedback with corrections
+        ### if you have any.
 else
-	sudo yum install -y $USRHOME/Downloads/$TV_FILENAME ### Not tested yet.  If you test it, and it doesn't work, please give me some feedback with corrections
-	### if you have any.
+	### Also not tested yet.  If you test it, and it doesn't work, please give me some feedback with corrections
+        ### if you have any.
+        sudo tar xvJf $USRHOME/Downloads/$TV_FILENAME -C /opt/
+        sudo ln -s /opt/teamviewer/teamviewer /usr/bin/teamviewer
+        sudo /opt/teamviewer/tv-setup checklibs
+        if [[ -z `sudo /opt/teamviewer/tv-setup checklibs | grep "All dependencies seem to be satisfied!"` ]]; then
+                echo "You must install the missing dependencies before you can run TeamViewer."
+                exit
+	fi
 fi
 sudo teamviewer --daemon enable
 sudo systemctl start teamviewerd
 
 echo ''
 echo "Teamviewer has been updated.  If you are still unable to open the GUI, try fixing any broken dependencies."
+exit 0
